529e7d40b248f822b3af687a5b258e4d6b4366a7
行者 <31663805+0xdeciverAngel@users.noreply.github.com>
2019-09-02 19:01:52 +0800

    update regexp

--- block.js
+++ block.js
@@ -1,7 +1,7 @@
 chrome.webRequest.onBeforeRequest.addListener(
   function(){ return {cancel: true}; },
   {
-    urls: ["*://ecsa.ntcu.edu.tw/Images/*"], // Change this to a more specific pattern
+    urls: ["*://ecsa.ntcu.edu.tw/Images/demo*"], // Change this to a more specific pattern
 
   },
   ["blocking"]
