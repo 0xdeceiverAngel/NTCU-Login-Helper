224a59c07426e2a07acef8e1c1ee7a587c32d9f3
行者 <31663805+0xdeciverAngel@users.noreply.github.com>
2019-11-28 02:41:37 +0800

    remove useless img tag

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 if (document.title.indexOf("國立臺中教育大學校園資訊系統") != -1) {
+
   var arr="";
   var i=0;
   for (i=0;i<5;i++)
@@ -11,16 +12,9 @@ if (document.title.indexOf("國立臺中教育大學校園資訊系統") != -1) 
     script.textContent = code;
     (document.head||document.documentElement).appendChild(script);
 
+    $(".flex.block").remove();
 
-    // var img=document.createElement("img");
-    // img.className="injimg";
-    // img.src = "https://upup.rf.gd/file/logo.jpg";
-    // var find=document.getElementsByClassName("banner")[0].appendChild(img);
-    // console.log(find);
-    var ico=document.createElement("link");
-    ico.rel="icon";
-    ico.href="https://upup.rf.gd/file/icon.ico";
-    ico.type="image/x-icon";
-    (document.head||document.documentElement).appendChild(ico);
+
+
 
   }
